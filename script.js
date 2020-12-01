9e145d2d56d26aeb8e1275bdf5160017a35cee3e
Alegherix <psao69@gmail.com>
2020-12-01 19:29:34 +0100

    working start animations

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const master = gsap.timeline();
 const container = document.querySelector('.animationContainer');
+const img = document.querySelector('.elonImg');
+const text = document.querySelector('.descriptionText');
 
-function createStartingText() {
-  const textNode = document.createElement('p');
-  textNode.textContent = 'There once was a young boy';
-  textNode.classList = 'descriptionText';
-  container.appendChild(textNode);
+function setStartingText() {
+  text.textContent = 'There once was a young boy';
 }
 
 function baseAnimation() {
@@ -20,39 +19,74 @@ function baseAnimation() {
   });
   return tl;
 }
-function animateAndUpdateText(text) {
-  const tl = gsap.timeline();
+
+function animateAndUpdateText(inputText) {
+  const tl = gsap.timeline({});
   tl.set('.descriptionText', {
-    text: text,
+    text: inputText,
   });
   tl.add(baseAnimation());
   return tl;
 }
-function appendImageToContainer() {
-  const img = document.querySelector('.elonImg');
-  const text = document.querySelector('.descriptionText');
-  img.src = './assets/elonAtEarlyAge.jpg';
-  img.classList.add('active');
-  text.classList.add('gone');
-}
 
 function animateElon() {
-  const tl = gsap.timeline({ onStart: appendImageToContainer });
+  const tl = gsap.timeline({
+    onStart: () => {
+      img.classList.toggle('gone');
+      text.classList.toggle('gone');
+    },
+    onComplete: () => {
+      img.classList.toggle('gone');
+      text.classList.toggle('gone');
+      img.src = '';
+    },
+  });
   tl.to('.elonImg', {
     opacity: 1,
     duration: 1.2,
     ease: Power0.easeNone,
+  }).to('.elonImg', {
+    opacity: 0,
+    delay: 0.5,
+    duration: 1.2,
+  });
+  return tl;
+}
+
+function animateElonSecond() {
+  const tl = gsap.timeline({
+    onStart: () => {
+      img.src = './assets/memelord.jpg';
+      img.classList.toggle('gone');
+      text.classList.toggle('gone');
+    },
+    onComplete: () => {
+      img.classList.toggle('gone');
+      text.classList.toggle('gone');
+    },
+  });
+  tl.to('.elonImg', {
+    opacity: 1,
+    duration: 1.2,
+    ease: Power0.easeNone,
+  }).to('.elonImg', {
+    opacity: 0,
+    delay: 0.5,
+    duration: 1.2,
   });
   return tl;
 }
 
 // Run all logic
 function main() {
-  createStartingText();
+  setStartingText();
   master
     .add(baseAnimation())
-    .add(animateAndUpdateText('Whom loved to party'))
-    .add(animateElon());
+    .add(animateAndUpdateText('He loved to party'))
+    .add(animateElon())
+    .add(animateAndUpdateText('You probably know his name'))
+    .add(animateAndUpdateText('ELON MUSK'))
+    .add(animateElonSecond());
 }
 
 main();
